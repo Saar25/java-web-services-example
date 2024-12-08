35a823852a354d8a63df3936276a6caa368e6c2f
Saar Tako <saartako25@gmail.com>
2024-12-08 23:43:41 +0200

    using arrays.aslist to make storage immutable

--- rest-service/src/main/java/org/saartako/AnimalController.java
+++ rest-service/src/main/java/org/saartako/AnimalController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class AnimalController {
 
-    private final List<Animal> animals = List.of(
+    private final List<Animal> animals = Arrays.asList(
         new Animal("Dog", "Bobby", 10),
         new Animal("Cat", "Meow", 5),
         new Animal("Cat", "Chubby", 6),
